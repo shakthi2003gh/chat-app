ef3ef88cd776b9c109482129f6449f3aac95c6dc
shakthi <shakthikumar13456789@gmail.com>
2022-09-25 18:15:38 +0530

    Create header component

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="header">
+        <div className="title">Chat app</div>
+
+        <button>Log out</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
