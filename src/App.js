6448cfd81f037a9a8672c6b8813422b8bc6df7f0
shakthi <shakthikumar13456789@gmail.com>
2022-09-25 18:38:52 +0530

    Create message compose component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import Compose from "./components/compose";
 
 function App() {
   return (
     <>
       <Header />
+      <Compose />
     </>
   );
 }
---
+++ src/components/compose.jsx
@@ -0,0 +1,13 @@
+const Compose = () => {
+  return (
+    <div className="compose-container">
+      <div className="compose">
+        <input type="text" />
+
+        <button>send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Compose;
